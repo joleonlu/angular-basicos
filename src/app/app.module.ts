df548f318314356781012e95ae6e64f8c454ab4e
jose luis <joseluisleonluna@gmail.com>
2021-06-08 21:36:45 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+import { HeroeModule } from './heroes/heroes.module';
+import { contadorModule } from './contador/contador.Module';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroeModule,
+    contadorModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
